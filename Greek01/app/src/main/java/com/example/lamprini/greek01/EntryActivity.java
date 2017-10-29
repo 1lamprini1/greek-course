5bb3af76b30e70929cb044576212f96a2f4e40bc
Lamprini Georgatsou <lamprinigeo@gmail.com>
2017-10-29 16:56:33 +0200

    Initial commit.

---
+++ Greek01/app/src/main/java/com/example/lamprini/greek01/EntryActivity.java
@@ -0,0 +1,15 @@
+package com.example.lamprini.greek01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EntryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+
+    }
+
+}
